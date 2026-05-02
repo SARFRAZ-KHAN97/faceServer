51bfc05bbb3b4bb1d96f5422466030205804f643
SARFRAZ-KHAN97 <193322154+SARFRAZ-KHAN97@users.noreply.github.com>
2026-05-02 19:07:10 +0530

    isolated faceserver

--- routes/recognize.py
+++ routes/recognize.py
@@ -1,32 +1,13 @@
-from fastapi import APIRouter, UploadFile, Form, BackgroundTasks
-import httpx
+from fastapi import APIRouter, UploadFile, Form
 from routes.enroll import KNOWN_EMBEDDINGS
 from services.face_utils import extract_embeddings
 from services.compare_utils import is_match
 
 router = APIRouter()
 
-NODE_SERVER_URL = "http://4.194.252.156:4040/attd/mark-batch"
-
-
-async def post_attendance(lecture_id, present_students):
-    try:
-        async with httpx.AsyncClient() as client:
-            await client.post(
-                NODE_SERVER_URL,
-                json={
-                    "lectureId": lecture_id,
-                    "presentStudents": present_students
-                },
-                timeout=5
-            )
-    except:
-        pass
-
 
 @router.post("/recognize")
 async def recognize_student(
-    background_tasks: BackgroundTasks,
     lectureId: str = Form(...),
     file: UploadFile = None
 ):
@@ -40,7 +21,6 @@ async def recognize_student(
 
     try:
         faces = extract_embeddings(image_bytes)
-
     except Exception as e:
         return {
             "status": "failure",
@@ -57,36 +37,28 @@ async def recognize_student(
     results = []
 
     for face in faces:
-        match_id, score = is_match(
-            face["embedding"],
-            KNOWN_EMBEDDINGS
-        )
+        embedding = face["embedding"]
+        bbox = face["bbox"]
+
+        match_id, score = is_match(embedding, KNOWN_EMBEDDINGS)
 
         if match_id:
             present_students.append(match_id)
-
             results.append({
                 "studentId": match_id,
                 "similarity": score,
-                "bbox": face["bbox"]
+                "bbox": bbox
             })
-
         else:
             results.append({
                 "studentId": None,
                 "similarity": None,
-                "bbox": face["bbox"],
+                "bbox": bbox,
                 "message": "No match found"
             })
 
-    background_tasks.add_task(
-        post_attendance,
-        lectureId,
-        present_students
-    )
-
     return {
         "status": "success",
-        "recognized": results,
-        "attendanceUpdate": "Processing in background"
+        "presentStudents": list(set(present_students)),
+        "recognized": results
     }
